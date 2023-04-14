df760d61decb53112602971d66e71201273bd0fb
suinesia <118024669+suinesia@users.noreply.github.com>
2023-04-14 15:45:40 +0700

    Create example.ts

---
+++ example.ts
@@ -0,0 +1,38 @@
+//update the data following the id that you have generated
+{
+
+    id: "xxxxx-xx-xxx-xxx-xxxxx",
+
+    name: "Project-Full-Name-Here",
+
+    shortName: "Project-Short-Name-Here",
+
+    description:
+
+      "Project-Descriptions",
+
+    tags: ["PROJECT-TAG"],
+
+    image: "logo-url",
+
+    network: {
+
+      website: "project-link",
+
+      github: "project-link",
+
+      twitter: "project-link",
+
+      blog: "project-link",
+
+      discord: "project-link",
+
+      telegram: "project-link",
+
+    },
+
+    isDevnetLive: falsefalfalseffalsefalfalsfalse,
+
+    isTestnetLive: true,
+
+  },
